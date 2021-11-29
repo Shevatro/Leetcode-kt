e5cfb1d3498e7b16ed6b463362db6b1b3b74bec4
Sergio Shevatro <sergio.shevatro@gmail.com>
2021-11-29 20:51:47 +0200

    Solved 875

---
+++ src/main/kotlin/lc800_899/Task875.kt
@@ -0,0 +1,54 @@
+package lc800_899
+
+import kotlin.math.ceil
+
+class Task875 {
+    fun minEatingSpeedRecursion(piles: IntArray, h: Int): Int {
+        val start = 1
+        val end = piles.maxOrNull() ?: -1
+        return minEatingSpeedRecursion(piles, h, start, end)
+    }
+
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        var start = 1
+        var end = piles.maxOrNull() ?: -1
+        while (end >= start) {
+            if (start == end) return start
+            val middle = (start + end) / 2
+            if (canBeEatenInTime(piles, h, middle)) {
+                end = middle
+            } else {
+                start = middle + 1
+            }
+        }
+        return -1
+    }
+
+    private fun minEatingSpeedRecursion(piles: IntArray, h: Int, start: Int, end: Int): Int {
+        val middle = (start + end) / 2
+        println("start:" + start + " end:" + end + " middle:" + middle)
+        if (end == start) return start
+        if (start > end) return -1
+        return if (canBeEatenInTime(piles, h, middle)) {
+            minEatingSpeedRecursion(piles, h, start, middle)
+        } else {
+            minEatingSpeedRecursion(piles, h, middle + 1, end)
+        }
+    }
+
+    private fun canBeEatenInTime(piles: IntArray, h: Int, k: Int): Boolean {
+        var sum = 0
+        for (pile in piles) {
+            sum += ceil(pile.toDouble() / k).toInt()
+        }
+        return sum <= h
+    }
+}
+
+fun main() {
+    val obj = Task875()
+    println(obj.minEatingSpeed(intArrayOf(3, 6, 7, 11), 8))
+    println(obj.minEatingSpeed(intArrayOf(30, 11, 23, 4, 20), 5))
+    println(obj.minEatingSpeed(intArrayOf(30, 11, 23, 4, 20), 6))
+    println(obj.minEatingSpeed(intArrayOf(3, 6, 7, 10), 8))
+}
