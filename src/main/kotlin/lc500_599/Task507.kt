d1e56e29c682b36095a53fbaa5ff1e053902914a
Sergio Shevatro <sergio.shevatro@gmail.com>
2021-11-29 18:09:03 +0200

    First commit

---
+++ src/main/kotlin/lc500_599/Task507.kt
@@ -0,0 +1,20 @@
+package lc500_599
+//Black list
+//https://leetcode.com/problems/perfect-number/
+
+class Task507 {
+    fun checkPerfectNumber(num: Int): Boolean {
+        var sum = 0
+        for (curNum in num - 1 downTo 1) {
+            if (num % curNum == 0) {
+                sum += curNum
+            }
+        }
+        return sum == num
+    }
+}
+
+fun main() {
+    val obj = Task507()
+    println(obj.checkPerfectNumber(2))
+}
