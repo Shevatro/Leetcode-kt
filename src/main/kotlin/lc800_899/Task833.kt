5675c162771f24ba4480366c6ac8f1b569327fea
Sergio Shevatro <sergio@rhythmic-rebellion.com>
2021-12-03 11:34:27 +0200

    Corrected 833

--- src/main/kotlin/lc800_899/Task833.kt
+++ src/main/kotlin/lc800_899/Task833.kt
@@ -1,30 +1,32 @@
 package lc800_899
+//Solved, but repeat
+//https://leetcode.com/problems/find-and-replace-in-string/
 
 class Task833 {
-    //Wrong
     fun findReplaceString(s: String, indices: IntArray, sources: Array<String>, targets: Array<String>): String {
-        val chars = mutableListOf<Char>()
-        var i = 0
-        var j = 0
-        while (i < s.length) {
-            if (i == indices.getOrNull(j)) {
-                val size = sources[j].length
-                val s1 = s.subSequence(i, i + size)
-                val s2 = sources[j]
-                if (s1 == s2) {
-                    chars.addAll(targets[j].toList())
-                    i += size
-                } else {
-                    chars.addAll(s1.toList())
-                    i += s1.length
-                }
-                j++
-            } else {
-                chars.add(s[i])
-                i++
+        var str = s
+        for (i in indices.indices) {
+            if (isStrsEqual(str, indices[i], sources[i])) {
+                val diff = targets[i].length - sources[i].length
+                str = replace(str, indices[i], indices[i] + sources[i].length, targets[i])
+                updateIndices(indices, i, diff)
             }
         }
-        return String(chars.toCharArray())
+        return str
+    }
+
+    private fun isStrsEqual(s: String, index: Int, findStr: String): Boolean {
+        return s.subSequence(index, index + findStr.length) == findStr
+    }
+
+    private fun replace(s: String, startIndex: Int, endIndex: Int, target: String): String {
+        return s.subSequence(0, startIndex).toString() + target + s.subSequence(endIndex, s.length)
+    }
+
+    private fun updateIndices(indices: IntArray, index: Int, diff: Int) {
+        for (i in index + 1 until indices.size) {
+            if (indices[i] > indices[index]) indices[i] += diff
+        }
     }
 }
 
@@ -32,5 +34,9 @@ fun main() {
     val obj = Task833()
     println(obj.findReplaceString("abcd", intArrayOf(0, 2), arrayOf("a", "cd"), arrayOf("eee", "ffff")))
     println(obj.findReplaceString("abcd", intArrayOf(0, 2), arrayOf("ab", "ec"), arrayOf("eee", "ffff")))
-    println(obj.findReplaceString("vmokgggqzp", intArrayOf(3, 5, 1), arrayOf("kg","ggq","mo"), arrayOf("s","so","bfr")))
+    println(
+        obj.findReplaceString(
+            "vmokgggqzp", intArrayOf(3, 5, 1), arrayOf("kg", "ggq", "mo"), arrayOf("s", "so", "bfr")
+        )
+    )
 }
