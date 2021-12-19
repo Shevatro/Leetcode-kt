307f402f45deedb62410c44f2dd9b30a20efada7
Sergio Shevatro <sergio.shevatro@gmail.com>
2021-12-19 11:54:26 +0200

    Resolved 1971

--- src/main/kotlin/lc1900_1999/Task1971.kt
+++ src/main/kotlin/lc1900_1999/Task1971.kt
@@ -2,9 +2,39 @@ package lc1900_1999
 //From a learning section, Solved
 //https://leetcode.com/problems/find-if-path-exists-in-graph/
 import common.UnionFind
+import toString2
+import java.util.*
 
 class Task1971 {
+
     fun validPath(n: Int, edges: Array<IntArray>, start: Int, end: Int): Boolean {
+        if (edges.isEmpty() && start == end) return true
+        val graph = createGraph(n, edges)
+        val stack = ArrayDeque<Node>()
+        stack.push(graph[start])
+        while (stack.isNotEmpty()) {
+            val node = stack.pop()
+            if (node.isVisited) continue
+            node.isVisited = true
+            for (subNodeInd in node.nodes) {
+                val subNode = graph[subNodeInd]
+                if (subNode.name == end) return true
+                stack.push(subNode)
+            }
+        }
+        return false
+    }
+
+    private fun createGraph(n: Int, edges: Array<IntArray>): Array<Node> {
+        val nodes = Array(n) { i -> Node(i) }
+        for (edge in edges) {
+            nodes[edge[0]].nodes.add(edge[1])
+            nodes[edge[1]].nodes.add(edge[0])
+        }
+        return nodes
+    }
+
+    fun validPathUsingUnionFind(n: Int, edges: Array<IntArray>, start: Int, end: Int): Boolean {
         val uf = createUnionFind(n, edges)
         return uf.isConnected(start, end)
     }
@@ -19,9 +49,26 @@ class Task1971 {
 
 }
 
+class Node(
+    val name: Int,
+    val nodes: MutableList<Int> = mutableListOf(),
+    var isVisited: Boolean = false
+
+) {
+    override fun toString(): String {
+        return name.toString() + " " + nodes.toString()
+    }
+}
+
 fun main() {
     val task = Task1971()
     println(task.validPath(3, arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(2, 0)), 0, 2))
     val edges2 = arrayOf(intArrayOf(0, 1), intArrayOf(0, 2), intArrayOf(3, 5), intArrayOf(5, 4), intArrayOf(4, 3))
+    val edges3 = arrayOf(
+        intArrayOf(4, 3), intArrayOf(1, 4), intArrayOf(4, 8), intArrayOf(1, 7), intArrayOf(6, 4), intArrayOf(4, 2),
+        intArrayOf(7, 4), intArrayOf(4, 0), intArrayOf(0, 9), intArrayOf(5, 4)
+    )
     println(task.validPath(6, edges2, 0, 5))
+    println(task.validPath(1, emptyArray(), 0, 0))
+    println(task.validPath(10, edges3, 5, 9))
 }
