d3d2128203f49174e992960e7647483351280aa8
Sergio Shevatro <sergio@rhythmic-rebellion.com>
2021-12-20 19:32:59 +0200

    Created a temple 332

---
+++ src/main/kotlin/lc1_999/lc300_399/Task332.kt
@@ -0,0 +1,19 @@
+package lc1_999.lc300_399
+
+//From a learning section, template
+//https://leetcode.com/problems/reconstruct-itinerary/
+class Task332 {
+    fun findItinerary(tickets: List<List<String>>): List<String> {
+
+    }
+
+    private fun createGraph(): MutableMap<String, MutableList<String>> {
+        val map = mutableMapOf<String, MutableList<String>>()
+
+    }
+
+}
+
+fun main() {
+
+}
