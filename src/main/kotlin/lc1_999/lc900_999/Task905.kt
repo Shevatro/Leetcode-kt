96b3876d4aee6b64179e9635023715c265c1c1a4
Sergio Shevatro <sergio.shevatro@gmail.com>
2022-09-08 13:04:00 +0300

    Test Optimized 905

--- src/main/kotlin/lc1_999/lc900_999/Task905.kt
+++ src/main/kotlin/lc1_999/lc900_999/Task905.kt
@@ -34,15 +34,15 @@ private class Task905Test {
 
     @Test
     fun sortArrayByParity() {
-        sortArrayByParity(intArrayOf(4, 2, 1, 3), intArrayOf(3, 1, 2, 4))
+        sortArrayByParity(intArrayOf(3, 1, 2, 4), intArrayOf(4, 2, 1, 3))
         sortArrayByParity(intArrayOf(0), intArrayOf(0))
         sortArrayByParity(intArrayOf(3, 1, 5, 7), intArrayOf(3, 1, 5, 7))
         sortArrayByParity(intArrayOf(2, 4, 8, 10), intArrayOf(2, 4, 8, 10))
-        sortArrayByParity(intArrayOf(4, 7, 5, 7), intArrayOf(7, 4, 5, 7))
-        sortArrayByParity(intArrayOf(4, 6, 7), intArrayOf(7, 6, 4))
+        sortArrayByParity(intArrayOf(7, 4, 5, 7), intArrayOf(4, 7, 5, 7))
+        sortArrayByParity(intArrayOf(7, 6, 4), intArrayOf(4, 6, 7))
     }
 
-    private fun sortArrayByParity(expected: IntArray, actualInp: IntArray) {
+    private fun sortArrayByParity(actualInp: IntArray, expected: IntArray) {
         val actual = task.sortArrayByParity(actualInp)
         println(actual.toString2())
         assertArrayEquals(expected, actual)
