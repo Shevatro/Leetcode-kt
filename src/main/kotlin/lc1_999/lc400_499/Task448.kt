81397ed31283124329dce4816d2f2d989561a98f
Sergio Shevatro <sergio.shevatro@gmail.com>
2022-09-09 13:49:30 +0300

    Solved 448

---
+++ src/main/kotlin/lc1_999/lc400_499/Task448.kt
@@ -0,0 +1,49 @@
+package lc1_999.lc400_499
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+//From a learning section, Solved
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+class Task448 {
+    fun findDisappearedNumbers(nums: IntArray): List<Int> {
+        fillAppeared(nums)
+        return getDisappearedList(nums)
+    }
+
+    private fun fillAppeared(nums: IntArray) {
+        for (num in nums) {
+            var index = num - 1
+            while (index >= 0) {
+                val temp = nums[index] - 1
+                nums[index] = -1
+                index = temp
+            }
+        }
+    }
+
+    private fun getDisappearedList(nums: IntArray): List<Int> {
+        val list = mutableListOf<Int>()
+        for (i in nums.indices) {
+            if (nums[i] != -1) list.add(i + 1)
+        }
+        return list
+    }
+}
+
+private class Task448Test {
+    private val task = Task448()
+
+    @Test
+    fun findDisappearedNumbers() {
+        findDisappearedNumbers(intArrayOf(4, 3, 2, 7, 8, 2, 3, 1), listOf(5, 6))
+        findDisappearedNumbers(intArrayOf(1, 1), listOf(2))
+        findDisappearedNumbers(intArrayOf(1), emptyList())
+    }
+
+    private fun findDisappearedNumbers(actualInp: IntArray, expected: List<Int>) {
+        val actual = task.findDisappearedNumbers(actualInp)
+        println(actual)
+        Assertions.assertEquals(expected, actual)
+    }
+}
