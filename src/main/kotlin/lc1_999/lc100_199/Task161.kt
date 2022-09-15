405def5c796385a804e1e2c0c30328994a5251a4
Sergio Shevatro <sergio.shevatro@gmail.com>
2022-09-15 10:01:07 +0300

    Solved 161

---
+++ src/main/kotlin/lc1_999/lc100_199/Task161.kt
@@ -0,0 +1,72 @@
+package lc1_999.lc100_199
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+
+//From Cracking The Coding Interview, Solved
+//https://leetcode.com/problems/one-edit-distance/
+
+class Task161 {
+    fun isOneEditDistance(s: String, t: String): Boolean {
+        if (s.length == t.length) {
+            var diffAmount = 0
+            for (i in s.indices) {
+                if (s[i] != t[i]) {
+                    diffAmount++
+                }
+                if (diffAmount > 1) {
+                    return false
+                }
+            }
+            return diffAmount == 1
+        } else {
+            if (abs(s.length - t.length) > 1) {
+                return false
+            }
+            val isFirstStrLonger = s.length > t.length
+            val strLong = if (isFirstStrLonger) s else t
+            val strShort = if (isFirstStrLonger) t else s
+            var j = 0
+            var diffAmount = 0
+            for (i in strLong.indices) {
+                if (j >= strShort.length) {
+                    diffAmount++
+                    break
+                }
+                if (strLong[i] == strShort[j]) {
+                    j++
+                } else {
+                    diffAmount++
+                }
+                if (diffAmount > 1) {
+                    return false
+                }
+            }
+            return diffAmount == 1
+        }
+    }
+
+}
+
+private class Task161Test {
+    private val task = Task161()
+
+    @Test
+    fun isOneEditDistance() {
+        isOneEditDistance("ab", "acb", true)
+        isOneEditDistance("", "", false)
+        isOneEditDistance("ab", "bcb", false)
+        isOneEditDistance("a", "A", true)
+        isOneEditDistance("abc", "abc", false)
+        isOneEditDistance("abc", "", false)
+        isOneEditDistance("abc", "abcd", true)
+        isOneEditDistance("teacher", "detacher", false)
+    }
+
+    private fun isOneEditDistance(actualInp1: String, actualInp2: String, expected: Boolean) {
+        val actual = task.isOneEditDistance(actualInp1, actualInp2)
+        println(actual)
+        Assertions.assertEquals(expected, actual)
+    }
+}
