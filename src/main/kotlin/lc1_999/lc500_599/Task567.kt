e4d3bddba107a3df084aebcd4c6a4776fb4be834
Sergio Shevatro <sergio.shevatro@gmail.com>
2024-08-02 16:37:36 -0700

    Improved 567

--- src/main/kotlin/lc1_999/lc500_599/Task567.kt
+++ src/main/kotlin/lc1_999/lc500_599/Task567.kt
@@ -8,32 +8,38 @@ import org.junit.jupiter.api.Test
 class Task567 {
     fun checkInclusion(s1: String, s2: String): Boolean {
         if (s1.length > s2.length) return false
-        val frequencyS1 = IntArray(26)
-        populateFrequency(frequencyS1, s1)
-        for (i in s2.indices) {
-            val frequencyS2 = IntArray(26)
-            try {
-                val s2Sub = s2.substring(i, s1.length + i)
-                populateFrequency(frequencyS2, s2Sub)
-                if (match(frequencyS1, frequencyS2)) return true
-            } catch (e: IndexOutOfBoundsException) {
-                return false
-            }
+        val (frequencyS1, frequencyS2) = populateInitState(s1, s2)
+        var amountNotEqual = countNotEqual(frequencyS1, frequencyS2)
+        if (amountNotEqual == 0) return true
+        for (i in 1..s2.length - s1.length) {
+            val previous = s2[i - 1] - 'a'
+            val next = s2[i + s1.length - 1] - 'a'
+            if (previous == next) continue
+            frequencyS2[previous]--
+            frequencyS2[next]++
+            amountNotEqual = countNotEqual(frequencyS1, frequencyS2)
+            if (amountNotEqual == 0) return true
         }
         return false
     }
 
-    private fun populateFrequency(frequency: IntArray, s: String) {
-        for (ch in s) {
-            frequency[ch - 'a']++
+    private fun populateInitState(s1: String, s2: String): Pair<IntArray, IntArray> {
+        val frequencyS1 = IntArray(26)
+        val frequencyS2 = IntArray(26)
+        for (i in s1.indices) {
+            frequencyS1[s1[i] - 'a']++
+            frequencyS2[s2[i] - 'a']++
         }
+        return frequencyS1 to frequencyS2
     }
 
-    private fun match(frequencyS1: IntArray, frequencyS2: IntArray): Boolean {
+
+    private fun countNotEqual(frequencyS1: IntArray, frequencyS2: IntArray): Int {
+        var count = 0
         for (i in frequencyS1.indices) {
-            if (frequencyS1[i] != frequencyS2[i]) return false
+            if (frequencyS1[i] != frequencyS2[i]) count++
         }
-        return true
+        return count
     }
 }
 
@@ -47,5 +53,6 @@ private class Task567Test {
         Assertions.assertEquals(true, task.checkInclusion("eidboaoo", "eidboaoo"))
         Assertions.assertEquals(true, task.checkInclusion("oidboaoe", "eidboaoo"))
         Assertions.assertEquals(false, task.checkInclusion("hello", "ooolleoooleh"))
+        Assertions.assertEquals(true, task.checkInclusion("adc", "dcda"))
     }
 }
