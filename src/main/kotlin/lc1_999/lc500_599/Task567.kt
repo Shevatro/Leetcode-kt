b531645faeeaed850809b4673437f0ba318949e7
Sergio Shevatro <sergio.shevatro@gmail.com>
2024-08-07 16:01:31 -0700

    Improved 567

--- src/main/kotlin/lc1_999/lc500_599/Task567.kt
+++ src/main/kotlin/lc1_999/lc500_599/Task567.kt
@@ -7,46 +7,58 @@ import org.junit.jupiter.api.Test
 //https://leetcode.com/problems/permutation-in-string/
 class Task567 {
     fun checkInclusion(s1: String, s2: String): Boolean {
-        if (s1.length > s2.length) return false
-        val (frequencyS1, frequencyS2) = populateInitState(s1, s2)
-        var amountNotEqual = countNotEqual(frequencyS1, frequencyS2)
-        if (amountNotEqual == 0) return true
-        for (i in 1..s2.length - s1.length) {
-            val previous = s2[i - 1] - 'a'
-            val next = s2[i + s1.length - 1] - 'a'
-            if (previous == next) continue
-            val previousDifferentNotEqual = frequencyS1[previous] != frequencyS2[previous]
-            val nextDifferentNotEqual = frequencyS1[next] != frequencyS2[next]
-            frequencyS2[previous]--
-            frequencyS2[next]++
-            val previousDifferentAfterNotEqual = frequencyS1[previous] != frequencyS2[previous]
-            val nextDifferentAfterNotEqual = frequencyS1[next] != frequencyS2[next]
-            if (previousDifferentNotEqual && !previousDifferentAfterNotEqual) amountNotEqual--
-            if (!previousDifferentNotEqual && previousDifferentAfterNotEqual) amountNotEqual++
-            if (nextDifferentNotEqual && !nextDifferentAfterNotEqual) amountNotEqual--
-            if (!nextDifferentNotEqual && nextDifferentAfterNotEqual) amountNotEqual++
+        return Solution(s1, s2).solve()
+    }
+
+    private class Solution(private val s1: String, private val s2: String) {
+        private val frequencyS1 = IntArray(26)
+        private val frequencyS2 = IntArray(26)
+        private var amountNotEqual = 0
+
+        fun solve(): Boolean {
+            if (s1.length > s2.length) return false
+            populateInitState()
+            amountNotEqual = countNotEqual()
             if (amountNotEqual == 0) return true
+            for (i in 1..s2.length - s1.length) {
+                val previousItem = s2[i - 1] - 'a'
+                val newItem = s2[i + s1.length - 1] - 'a'
+                if (previousItem == newItem) continue
+                shiftFrequency(previousItem, -1)
+                shiftFrequency(newItem, 1)
+                if (amountNotEqual == 0) return true
+            }
+            return false
         }
-        return false
-    }
 
-    private fun populateInitState(s1: String, s2: String): Pair<IntArray, IntArray> {
-        val frequencyS1 = IntArray(26)
-        val frequencyS2 = IntArray(26)
-        for (i in s1.indices) {
-            frequencyS1[s1[i] - 'a']++
-            frequencyS2[s2[i] - 'a']++
+        private fun populateInitState() {
+            for (i in s1.indices) {
+                frequencyS1[s1[i] - 'a']++
+                frequencyS2[s2[i] - 'a']++
+            }
         }
-        return frequencyS1 to frequencyS2
-    }
 
 
-    private fun countNotEqual(frequencyS1: IntArray, frequencyS2: IntArray): Int {
-        var count = 0
-        for (i in frequencyS1.indices) {
-            if (frequencyS1[i] != frequencyS2[i]) count++
+        private fun countNotEqual(): Int {
+            var count = 0
+            for (i in frequencyS1.indices) {
+                if (isFrequencyDifferent(i)) count++
+            }
+            return count
         }
-        return count
+
+        private fun isFrequencyDifferent(item: Int): Boolean {
+            return frequencyS1[item] != frequencyS2[item]
+        }
+
+        private fun shiftFrequency(item: Int, frequencyDiff: Int) {
+            val diffFrequencyIndicatorBefore = isFrequencyDifferent(item).toInt()
+            frequencyS2[item] += frequencyDiff
+            val diffFrequencyIndicatorAfter = isFrequencyDifferent(item).toInt()
+            amountNotEqual += diffFrequencyIndicatorAfter - diffFrequencyIndicatorBefore
+        }
+
+        private fun Boolean.toInt() = if (this) 1 else 0
     }
 }
 
