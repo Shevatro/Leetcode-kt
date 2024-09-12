183ad47844140ac764e74952d5d8ce809e4c5c5d
Sergio Shevatro <sergio.shevatro@gmail.com>
2024-09-12 15:18:05 -0700

    Added 2th solution for 155

--- src/main/kotlin/lc1_999/lc100_199/Task155.kt
+++ src/main/kotlin/lc1_999/lc100_199/Task155.kt
@@ -2,30 +2,55 @@ package lc1_999.lc100_199
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
 
 //From a learning section, Solved but with a hint
 //https://leetcode.com/problems/min-stack/
 class Task155 {
-    private var top: Node? = null
-    fun push(`val`: Int) {
-        val previousTop = top
-        val min = min(previousTop?.min ?: Int.MAX_VALUE, `val`)
-        top = Node(`val`, min, previousTop)
+    class Solution1 {
+        private var top: Node? = null
+        fun push(`val`: Int) {
+            val previousTop = top
+            val min = min(previousTop?.min ?: Int.MAX_VALUE, `val`)
+            top = Node(`val`, min, previousTop)
+        }
+
+        fun pop() {
+            top = top?.next
+        }
+
+        fun top(): Int {
+            return requireNotNull(top).`val`
+        }
+
+        fun getMin(): Int {
+            return requireNotNull(top).min
+        }
     }
 
-    fun pop() {
-        top = top?.next
+    class Solution2 {
+        private val stack = ArrayDeque<Node>()
+        fun push(`val`: Int) {
+            val previousTop = stack.firstOrNull()
+            val min = min(previousTop?.min ?: Int.MAX_VALUE, `val`)
+            stack.addFirst(Node(`val`, min, previousTop))
+        }
+
+        fun pop() {
+            stack.removeFirst()
+        }
+
+        fun top(): Int {
+            return stack.first().`val`
+        }
+
+        fun getMin(): Int {
+            return stack.first().min
+        }
     }
 
-    fun top(): Int {
-        return requireNotNull(top).`val`
-    }
-
-    fun getMin(): Int {
-        return requireNotNull(top).min
-    }
 
     private class Node(
         val `val`: Int,
@@ -35,10 +60,21 @@ class Task155 {
 }
 
 private class Task155Test {
-    private val task = Task155()
+    @Test
+    fun minStackTest() {
+        val task = Task155.Solution1()
+        task.push(-2)
+        task.push(0)
+        task.push(-3)
+        Assertions.assertEquals(-3, task.getMin())
+        task.pop()
+        Assertions.assertEquals(0, task.top())
+        Assertions.assertEquals(-2, task.getMin())
+    }
 
     @Test
-    fun minStack() {
+    fun minStackTest2() {
+        val task = Task155.Solution2()
         task.push(-2)
         task.push(0)
         task.push(-3)
