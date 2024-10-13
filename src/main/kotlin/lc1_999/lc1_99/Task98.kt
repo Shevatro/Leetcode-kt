1c62d94ed1dc5c7b81e23b1b5007b82d07d805fe
Sergio Shevatro <sergio.shevatro@gmail.com>
2024-10-13 14:18:15 -0700

    Added the second solution for 98

--- src/main/kotlin/lc1_999/lc1_99/Task98.kt
+++ src/main/kotlin/lc1_999/lc1_99/Task98.kt
@@ -7,21 +7,56 @@ import org.junit.jupiter.api.Test
 //From Cracking The Coding Interview, Not Solved
 //https://leetcode.com/problems/validate-binary-search-tree/
 class Task98 {
-    private var previousValue: Int? = null
     fun isValidBST(root: IntTreeNode?): Boolean {
-        previousValue = null
-        return isValid(root)
+        return Solution().isValidBST(root)
+//        return Solution2().isValidBST(root)
     }
 
-    private fun isValid(root: IntTreeNode?): Boolean {
-        if (root == null) return true
-        //left
-        if (!isValid(root.left)) return false
-        //current
-        if (!(previousValue == null || previousValue!! < root.`val`)) return false
-        previousValue = root.`val`
-        //right, if nobody violates the order -> return true
-        return isValid(root.right)
+    private class Solution {
+        private var previousValue: Int? = null
+        fun isValidBST(root: IntTreeNode?): Boolean {
+            previousValue = null
+            return isValid(root)
+        }
+
+        private fun isValid(root: IntTreeNode?): Boolean {
+            if (root == null) return true
+            //left
+            if (!isValid(root.left)) return false
+            //current
+            if (!(previousValue == null || previousValue!! < root.`val`)) return false
+            previousValue = root.`val`
+            //right, if nobody violates the order -> return true
+            return isValid(root.right)
+        }
+    }
+
+    private class Solution2 {
+        private var sortedList = ArrayList<Int>()
+        fun isValidBST(root: IntTreeNode?): Boolean {
+            sortedList = ArrayList()
+            traverseAndAddItems(root)
+            return checkIfSorted()
+        }
+
+        private fun traverseAndAddItems(root: IntTreeNode?) {
+            if (root == null) return
+            //left
+            traverseAndAddItems(root.left)
+            //current
+            sortedList.add(root.`val`)
+            //right
+            return traverseAndAddItems(root.right)
+        }
+
+        private fun checkIfSorted(): Boolean {
+            var previous: Int? = null
+            for (item in sortedList) {
+                if (previous != null && item <= previous) return false
+                previous = item
+            }
+            return true
+        }
     }
 }
 
@@ -56,5 +91,12 @@ private class Task110Test {
         }
         val result3 = task.isValidBST(tree3)
         Assertions.assertEquals(false, result3)
+
+        val tree4 = IntTreeNode(2).apply {
+            left = IntTreeNode(2)
+            right = IntTreeNode(2)
+        }
+        val result4 = task.isValidBST(tree4)
+        Assertions.assertEquals(false, result4)
     }
 }
