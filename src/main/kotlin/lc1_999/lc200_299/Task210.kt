8f5406a1eb89171c62fca58a9de88ede8062e52f
Sergio Shevatro <sergio.shevatro@gmail.com>
2024-12-01 17:30:00 -0800

    Finished another solution for 210

--- src/main/kotlin/lc1_999/lc200_299/Task210.kt
+++ src/main/kotlin/lc1_999/lc200_299/Task210.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 //https://leetcode.com/problems/course-schedule-ii/
 private class Task210 {
     fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
-        return Solution(numCourses, prerequisites).findOrder()
+        return Solution2(numCourses, prerequisites).findOrder()
     }
 
     private class Solution(private val numCourses: Int, private val prerequisites: Array<IntArray>) {
@@ -52,12 +52,58 @@ private class Task210 {
                 destinations[from]?.add(to)
             }
         }
+
+        private enum class STATE {
+            NONE, IN_PROGRESS, DONE
+        }
     }
 
-    private enum class STATE {
-        NONE, IN_PROGRESS, DONE
-    }
+    private class Solution2(private val numCourses: Int, private val prerequisites: Array<IntArray>) {
+        private val destinations = HashMap<Int, MutableSet<Int>>()
+        private val referencesRecord = IntArray(numCourses)
+        private val queue = ArrayDeque<Int>()
+        private val result = IntArray(numCourses)
+        private var courseCounter = 0
 
+        fun findOrder(): IntArray {
+            fillReverseDestinationMapAndReferences()
+            fillQueueWithNoReferenceItems()
+            performTopologicalSort()
+            return if (courseCounter == numCourses) result else intArrayOf()
+        }
+
+        private fun fillReverseDestinationMapAndReferences() {
+            for (prerequisite in prerequisites) {
+                val from = prerequisite[1]
+                val to = prerequisite[0]
+                if (destinations[from] == null) {
+                    destinations[from] = mutableSetOf()
+                }
+                destinations[from]?.add(to)
+                referencesRecord[to]++
+            }
+        }
+
+        private fun fillQueueWithNoReferenceItems() {
+            for (i in referencesRecord.indices) {
+                if (referencesRecord[i] == 0) queue.addLast(i)
+            }
+        }
+
+        private fun performTopologicalSort() {
+            while (queue.isNotEmpty()) {
+                val item = queue.removeFirst()
+                result[courseCounter] = item
+                courseCounter++
+                for (destination in destinations[item] ?: emptySet()) {
+                    referencesRecord[destination]--
+                    if (referencesRecord[destination] == 0) {
+                        queue.addLast(destination)
+                    }
+                }
+            }
+        }
+    }
 }
 
 private class Task210Test {
@@ -106,7 +152,17 @@ private class Task210Test {
     @Test
     fun findOrderTest6() {
         val input = arrayOf(intArrayOf(0, 2), intArrayOf(1, 0))
-        val expectedResult = intArrayOf(2, 0, 1, 3)
+        val expectedResult1 = intArrayOf(2, 0, 1, 3)
+        val expectedResult2 = intArrayOf(2, 3, 0, 1)
+        val result = task.findOrder(4, input)
+        val isValid = result.contentEquals(expectedResult1) || result.contentEquals(expectedResult2)
+        Assertions.assertEquals(true, isValid)
+    }
+
+    @Test
+    fun findOrderTest7() {
+        val input = arrayOf(intArrayOf(0, 1), intArrayOf(1, 3), intArrayOf(0, 2), intArrayOf(2, 3))
+        val expectedResult = intArrayOf(3, 1, 2, 0)
         val result = task.findOrder(4, input)
         Assertions.assertArrayEquals(expectedResult, result)
     }
