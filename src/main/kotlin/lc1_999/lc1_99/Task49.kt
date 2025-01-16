df1a979eb4f4333bf9072ac691eaa069c638935d
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-01-15 17:15:32 -0800

    Solved 49

---
+++ src/main/kotlin/lc1_999/lc1_99/Task49.kt
@@ -0,0 +1,72 @@
+package lc1_999.lc1_99
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+//From Cracking The Coding Interview, Solved
+//https://leetcode.com/problems/group-anagrams/
+class Task49 {
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val result = mutableListOf<MutableList<String>>()
+        val map = mutableMapOf<String, Int>()
+        for (str in strs) {
+            val sortedStr = str.toCharArray().sorted().toString()
+            if (map[sortedStr] == null) {
+                result.add(mutableListOf())
+                map[sortedStr] = result.lastIndex
+            }
+            val index = map[sortedStr]!!
+            result[index].add(str)
+        }
+        return result
+    }
+}
+
+private class Task49Test {
+    private val task = Task49()
+
+    @Test
+    fun test1() {
+        val input = arrayOf("eat", "tea", "tan", "ate", "nat", "bat")
+        val actualResult = task.groupAnagrams(input)
+        val expectedResult = listOf(
+            listOf("bat"), listOf("nat", "tan"), listOf("ate", "eat", "tea")
+        )
+        Assertions.assertEquals(expectedResult.size, actualResult.size)
+        val expectedSorted = expectedResult.map { it.sorted() }.sortedBy { it[0] }
+        val actualSorted = actualResult.map { it.sorted() }.sortedBy { it[0] }
+        Assertions.assertEquals(expectedSorted, actualSorted)
+    }
+
+    @Test
+    fun test2() {
+        val input = arrayOf("")
+        val actualResult = task.groupAnagrams(input)
+        val expectedResult = listOf(listOf(""))
+        Assertions.assertEquals(expectedResult.size, actualResult.size)
+        Assertions.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun test3() {
+        val input = arrayOf("a")
+        val actualResult = task.groupAnagrams(input)
+        val expectedResult = listOf(listOf("a"))
+        Assertions.assertEquals(expectedResult.size, actualResult.size)
+        Assertions.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun test4() {
+        val input = arrayOf("eat", "tea", "tan", "eta", "nat", "bat")
+        val actualResult = task.groupAnagrams(input)
+        val expectedResult = listOf(
+            listOf("bat"), listOf("nat", "tan"), listOf("eta", "eat", "tea")
+        )
+        Assertions.assertEquals(expectedResult.size, actualResult.size)
+        val expectedSorted = expectedResult.map { it.sorted() }.sortedBy { it[0] }
+        val actualSorted = actualResult.map { it.sorted() }.sortedBy { it[0] }
+        Assertions.assertEquals(expectedSorted, actualSorted)
+    }
+}
