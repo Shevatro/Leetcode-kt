351857619c94f89142c6f809d8064288f8663fec
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-01-15 20:57:53 -0800

    Added more solutions for 49

--- src/main/kotlin/lc1_999/lc1_99/Task49.kt
+++ src/main/kotlin/lc1_999/lc1_99/Task49.kt
@@ -2,13 +2,14 @@ package lc1_999.lc1_99
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.TreeMap
 
 //From Cracking The Coding Interview, Solved
 //https://leetcode.com/problems/group-anagrams/
 class Task49 {
 
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        return Solution2(strs).groupAnagrams()
+        return Solution4(strs).groupAnagrams()
     }
 
     private class Solution() {
@@ -43,6 +44,75 @@ class Task49 {
                 }
                 map[sortedStr]?.add(str)
             }
+        }
+    }
+
+    private class Solution3(private val strs: Array<String>) {
+        private val map = mutableMapOf<String, MutableList<String>>()
+
+        fun groupAnagrams(): List<List<String>> {
+            fillInMap()
+            return ArrayList(map.values)
+        }
+
+        private fun fillInMap() {
+            for (str in strs) {
+                val sortedStr = generateHash(str)
+                if (map[sortedStr] == null) {
+                    map[sortedStr] = mutableListOf()
+                }
+                map[sortedStr]?.add(str)
+            }
+        }
+
+        private fun generateHash(str: String): String {
+            val frequency = fillInFrequency(str)
+            val sb = StringBuilder()
+            frequency.forEach { (key, value) -> sb.append(key).append(value) }
+            return sb.toString()
+        }
+
+        private fun fillInFrequency(str: String): Map<Char, Int> {
+            val frequency = TreeMap<Char, Int>()
+            for (ch in str) {
+                frequency[ch] = (frequency[ch] ?: 0) + 1
+            }
+            return frequency
+        }
+    }
+
+    private class Solution4(private val strs: Array<String>) {
+        private val map = mutableMapOf<String, MutableList<String>>()
+
+        fun groupAnagrams(): List<List<String>> {
+            fillInMap()
+            return ArrayList(map.values)
+        }
+
+        private fun fillInMap() {
+            for (str in strs) {
+                val sortedStr = generateHash(str)
+                if (map[sortedStr] == null) {
+                    map[sortedStr] = mutableListOf()
+                }
+                map[sortedStr]?.add(str)
+            }
+        }
+
+        private fun generateHash(str: String): String {
+            val frequency = fillInFrequency(str)
+            val sb = StringBuilder()
+            frequency.forEach { sb.append("#").append(it) }
+            return sb.toString()
+        }
+
+        private fun fillInFrequency(str: String): IntArray {
+            val frequency = IntArray(26)
+            for (ch in str) {
+                val code = ch - 'a'
+                frequency[code] = frequency[code] + 1
+            }
+            return frequency
         }
     }
 }
@@ -87,6 +157,19 @@ private class Task49Test {
         val actualResult = task.groupAnagrams(input)
         val expectedResult = listOf(
             listOf("bat"), listOf("nat", "tan"), listOf("eta", "eat", "tea")
+        )
+        Assertions.assertEquals(expectedResult.size, actualResult.size)
+        val expectedSorted = expectedResult.map { it.sorted() }.sortedBy { it[0] }
+        val actualSorted = actualResult.map { it.sorted() }.sortedBy { it[0] }
+        Assertions.assertEquals(expectedSorted, actualSorted)
+    }
+
+    @Test
+    fun test5() {
+        val input = arrayOf("bdddddddddd", "bbbbbbbbbbc")
+        val actualResult = task.groupAnagrams(input)
+        val expectedResult = listOf(
+            listOf("bbbbbbbbbbc"), listOf("bdddddddddd")
         )
         Assertions.assertEquals(expectedResult.size, actualResult.size)
         val expectedSorted = expectedResult.map { it.sorted() }.sortedBy { it[0] }
