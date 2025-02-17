1a03426dff8bdfc76aba6ea825a0c1944ed1a04b
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-02-17 15:52:04 -0800

    Added another solution for 146

--- src/main/kotlin/lc1_999/lc100_199/Task146.kt
+++ src/main/kotlin/lc1_999/lc100_199/Task146.kt
@@ -7,6 +7,55 @@ import org.junit.jupiter.api.Test
 //https://leetcode.com/problems/lru-cache/description/
 class Task146 {
     class LRUCache(private val capacity: Int) {
+        private var head = Node(-1, -1)
+        private var tail = head
+        private val map = HashMap<Int, Node>()
+        fun get(key: Int): Int {
+            val node = map[key] ?: return -1
+            if (node != tail) {
+                removeNode(node)
+                addNode(key, node.value)
+            }
+            return node.value
+        }
+
+        fun put(key: Int, value: Int) {
+            if (map[key] != null) {
+                removeNode(map[key]!!)
+            } else if (map.size == capacity) {
+                val node = head.next!!
+                val nodeKey = node.key
+                removeNode(head.next!!)
+                map.remove(nodeKey)
+            }
+            addNode(key, value)
+        }
+
+        private fun removeNode(node: Node) {
+            //don't want to lose a tail
+            if (node == tail) tail = tail.prev!!
+            val prev = node.prev
+            val next = node.next
+            prev?.next = next
+            next?.prev = prev
+        }
+
+        private fun addNode(key: Int, value: Int) {
+            val newNode = Node(key, value, tail)
+            tail.next = newNode
+            tail = newNode
+            map[key] = tail
+        }
+
+        private class Node(
+            val key: Int,
+            val value: Int,
+            var prev: Node? = null,
+            var next: Node? = null
+        )
+    }
+
+    class LRUCacheFirstSolution(private val capacity: Int) {
         private val map = mutableMapOf<Int, Int>()
         fun get(key: Int): Int {
             val temp = map[key] ?: return -1
@@ -87,6 +136,16 @@ private class Task146Test {
         }
         Assertions.assertEquals(-1, lruCache.get(1))
         Assertions.assertEquals(3, lruCache.get(2))
+    }
+
+    @Test
+    fun test4() {
+        val lruCache = Task146.LRUCache(1)
+        lruCache.put(2, 1)
+        Assertions.assertEquals(1, lruCache.get(2))
+        lruCache.put(3, 2)
+        Assertions.assertEquals(-1, lruCache.get(2))
+        Assertions.assertEquals(2, lruCache.get(3))
     }
 
 }
