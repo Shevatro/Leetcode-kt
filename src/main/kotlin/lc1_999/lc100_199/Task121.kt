12eabca4406c3d66c144d1d9a6a94a8d8aac1fe6
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-02-17 19:16:51 -0800

    Finished 121

---
+++ src/main/kotlin/lc1_999/lc100_199/Task121.kt
@@ -0,0 +1,54 @@
+package lc1_999.lc100_199
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.math.max
+import kotlin.math.min
+
+//Not Solved
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+class Task121 {
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        var minPrice = prices[0]
+        for (price in prices) {
+            maxProfit = max(maxProfit, price - minPrice)
+            minPrice = min(minPrice, price)
+        }
+        return maxProfit
+    }
+}
+
+private class Task121Test {
+    private val task = Task121()
+
+    @Test
+    fun test1() {
+        val result = task.maxProfit(intArrayOf(7, 1, 5, 3, 6, 4))
+        Assertions.assertEquals(5, result)
+    }
+
+    @Test
+    fun test2() {
+        val result = task.maxProfit(intArrayOf(7, 6, 4, 3, 1))
+        Assertions.assertEquals(0, result)
+    }
+
+    @Test
+    fun test3() {
+        val result = task.maxProfit(intArrayOf(7))
+        Assertions.assertEquals(0, result)
+    }
+
+    @Test
+    fun test4() {
+        val result = task.maxProfit(intArrayOf(7, 1))
+        Assertions.assertEquals(0, result)
+    }
+
+    @Test
+    fun test5() {
+        val result = task.maxProfit(intArrayOf(2, 4, 1))
+        Assertions.assertEquals(2, result)
+    }
+}
