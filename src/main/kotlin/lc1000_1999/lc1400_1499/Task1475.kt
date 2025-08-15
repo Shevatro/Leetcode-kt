8b2e20441bb4311849c63ee8e510ca61b370d371
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-08-15 15:06:09 -0700

    Optimized tests in 1475

--- src/main/kotlin/lc1000_1999/lc1400_1499/Task1475.kt
+++ src/main/kotlin/lc1000_1999/lc1400_1499/Task1475.kt
@@ -1,19 +1,22 @@
 package lc1000_1999.lc1400_1499
 
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
+import java.util.stream.Stream
 
 //Monotonic Stack, Not Solved
 //https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/
 class Task1475() {
     fun finalPrices(prices: IntArray): IntArray {
-        val stack = ArrayDeque<Int>()
         val result = prices.copyOf()
-        for (i in prices.indices) {
-            while (stack.isNotEmpty() && prices[i] <= prices[stack.peek()]) {
-                val prevIndex = stack.pop()
-                result[prevIndex] = prices[prevIndex] - prices[i]
+        val stack = ArrayDeque<Int>()
+        for (i in prices.indices){
+            while (stack.isNotEmpty() && prices[stack.peek()]>= prices[i]){
+                val index = stack.pop()
+                result[index] -= prices[i]
             }
             stack.push(i)
         }
@@ -24,31 +27,21 @@ class Task1475() {
 private class Task1475Test {
     private val task = Task1475()
 
-    @Test
-    fun test1() {
-        val input = intArrayOf(8, 4, 6, 2, 3)
-        val output = intArrayOf(4, 2, 4, 2, 3)
-        Assertions.assertArrayEquals(output, task.finalPrices(input))
+    @ParameterizedTest
+    @MethodSource("intArraysProvider")
+    fun test(input: IntArray, expected: IntArray) {
+        Assertions.assertArrayEquals(expected, task.finalPrices(input))
     }
 
-    @Test
-    fun test2() {
-        val input = intArrayOf(1, 2, 3, 4, 5)
-        val output = intArrayOf(1, 2, 3, 4, 5)
-        Assertions.assertArrayEquals(output, task.finalPrices(input))
-    }
-
-    @Test
-    fun test3() {
-        val input = intArrayOf(10, 1, 1, 6)
-        val output = intArrayOf(9, 0, 1, 6)
-        Assertions.assertArrayEquals(output, task.finalPrices(input))
-    }
-
-    @Test
-    fun test4() {
-        val input = intArrayOf(2, 3, 4, 5, 1)
-        val output = intArrayOf(1, 2, 3, 4, 1)
-        Assertions.assertArrayEquals(output, task.finalPrices(input))
+    companion object {
+        @JvmStatic
+        private fun intArraysProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(intArrayOf(8, 4, 6, 2, 3), intArrayOf(4, 2, 4, 2, 3)),
+                Arguments.of(intArrayOf(1, 2, 3, 4, 5), intArrayOf(1, 2, 3, 4, 5)),
+                Arguments.of(intArrayOf(10, 1, 1, 6), intArrayOf(9, 0, 1, 6)),
+                Arguments.of(intArrayOf(2, 3, 4, 5, 1), intArrayOf(1, 2, 3, 4, 1))
+            )
+        }
     }
 }
