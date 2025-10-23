4bf67215fb2df3f26fcfac53186338ca7443a9e9
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-10-23 14:03:15 -0700

    Improved 32.2

--- src/main/kotlin/bctci/stacks_and_queues/Task32.2.kt
+++ src/main/kotlin/bctci/stacks_and_queues/Task32.2.kt
@@ -7,40 +7,40 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class Task32_2() {
-
     fun compressArray(arr: IntArray, k: Int): IntArray {
-        val stack = ArrayDeque<Item>()
-        stack.addFirst(Item(arr[0]))
-        var i = 1
-        while (i != arr.size) {
-            val previousItem = stack.first()
-            if (previousItem.count == k) {
-                repeat(k) { stack.removeFirst() }
-                val item = previousItem.item * k
-                val count = if (stack.isNotEmpty() && stack.first().item == item) stack.first().count + 1 else 1
-
-                stack.addFirst(Item(item, count))
-                //found match and we need to revaluate
-                if (count != k) i++
-            } else if (previousItem.item == arr[i]) {
-                val newCount = previousItem.count + 1
-                stack.addFirst(Item(arr[i], newCount))
-                //found match and we need to revaluate
-                if (newCount != k) i++
-            } else {
-                stack.addFirst(Item(arr[i]))
-                i++
-            }
-        }
-        return convertStackToResultArr(stack)
+        return Solution(arr, k).compressArray()
     }
 
-    private fun convertStackToResultArr(stack: ArrayDeque<Item>): IntArray {
-        val resultArr = IntArray(stack.size)
-        for (i in resultArr.indices) {
-            resultArr[i] = stack.removeLast().item
+    class Solution(private val arr: IntArray, private val k: Int) {
+        private val stack = ArrayDeque<Item>()
+        fun compressArray(): IntArray {
+            for (item in arr) {
+                processItem(item)
+            }
+            return convertStackToResultArr(stack)
         }
-        return resultArr
+
+        private fun processItem(sum: Int) {
+            if (stack.isEmpty() || stack.first().item != sum) {
+                stack.addFirst(Item(sum))
+            } else if (stack.first().count < k - 1) {
+                stack.addFirst(Item(sum, stack.removeFirst().count + 1))
+            } else {
+                stack.removeFirst()
+                processItem(sum * k)
+            }
+        }
+
+        private fun convertStackToResultArr(stack: ArrayDeque<Item>): IntArray {
+            val resultArr = ArrayList<Int>()
+            while (stack.isNotEmpty()) {
+                val item = stack.removeLast()
+                repeat(item.count) {
+                    resultArr.add(item.item)
+                }
+            }
+            return resultArr.toIntArray()
+        }
     }
 
     private data class Item(
@@ -66,9 +66,9 @@ private class Task32_2Test {
                 Arguments.of(intArrayOf(8, 4, 2, 2), 2, intArrayOf(16)),
                 Arguments.of(intArrayOf(4, 4, 4, 4), 5, intArrayOf(4, 4, 4, 4)),
                 Arguments.of(intArrayOf(2, 2, 2), 2, intArrayOf(4, 2)),
-                Arguments.of(intArrayOf(2, 2, 2, 2), 3, intArrayOf(6,2)),
-                Arguments.of(intArrayOf(1,1,2,2,2,3,3,3,3), 3, intArrayOf(1,1,6,9,3)),
-                Arguments.of(intArrayOf(1,1,1,1,1,1), 3, intArrayOf(3,3))
+                Arguments.of(intArrayOf(2, 2, 2, 2), 3, intArrayOf(6, 2)),
+                Arguments.of(intArrayOf(1, 1, 2, 2, 2, 3, 3, 3, 3), 3, intArrayOf(1, 1, 6, 9, 3)),
+                Arguments.of(intArrayOf(1, 1, 1, 1, 1, 1), 3, intArrayOf(3, 3))
             )
         }
     }
