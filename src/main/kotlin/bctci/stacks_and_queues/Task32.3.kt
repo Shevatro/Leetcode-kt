624ba32bcb17e5a4107711a1c801affadb288a66
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-10-27 19:22:31 -0700

    Improved 32.3

--- src/main/kotlin/bctci/stacks_and_queues/Task32.3.kt
+++ src/main/kotlin/bctci/stacks_and_queues/Task32.3.kt
@@ -2,31 +2,47 @@ package bctci.stacks_and_queues
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+
 class Task32_3() {
     class ViewerCounter(private val window: Int) {
         private val viewers = mutableMapOf(
-            "subscriber" to ArrayDeque<Int>(),
-            "guest" to ArrayDeque(),
-            "follower" to ArrayDeque()
+            "subscriber" to Data(),
+            "guest" to Data(),
+            "follower" to Data()
         )
 
         fun join(t: Int, vt: String) {
             trimDeque(t, vt)
-            viewers[vt]?.addLast(t + window)
+            val newWindowSize = t + window
+            val item = requireNotNull(viewers[vt])
+            val deque = item.deque
+            if (deque.isEmpty() || deque.last().first != newWindowSize) {
+                deque.addLast(newWindowSize to 1)
+            } else {
+                val previousCount = deque.removeLast().second
+                deque.addLast(newWindowSize to previousCount + 1)
+            }
+            item.count++
         }
 
         fun getViewers(t: Int, vt: String): Int {
             trimDeque(t, vt)
-            return viewers[vt]?.size ?: 0
+            return viewers[vt]!!.count
         }
 
         private fun trimDeque(t: Int, vt: String) {
-            val deque = viewers[vt] ?: return
-            while (deque.isNotEmpty() && t > deque.first()) {
-                deque.removeFirst()
+            val item = viewers[vt] ?: return
+            val deque = item.deque
+            while (deque.isNotEmpty() && t > deque.first().first) {
+                item.count -= deque.removeFirst().second
             }
         }
     }
+
+    private data class Data(
+        val deque: ArrayDeque<Pair<Int, Int>> = ArrayDeque(),
+        var count: Int = 0
+    )
 }
 
 private class Task32_3Test {
@@ -40,8 +56,8 @@ private class Task32_3Test {
         counter.join(2, "follower")
         counter.join(2, "follower")
         counter.join(3, "follower")
-        Assertions.assertEquals(1,counter.getViewers(10, "subscriber"))
-        Assertions.assertEquals(1,counter.getViewers(10, "guest"))
+        Assertions.assertEquals(1, counter.getViewers(10, "subscriber"))
+        Assertions.assertEquals(1, counter.getViewers(10, "guest"))
         Assertions.assertEquals(4, counter.getViewers(10, "follower"))
         Assertions.assertEquals(1, counter.getViewers(13, "follower"))
     }
