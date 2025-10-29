94a273e6687f93a7867581c343b94b402057d23b
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-10-29 15:53:43 -0700

    Solved 32.7

---
+++ src/main/kotlin/bctci/stacks_and_queues/Task32.7.kt
@@ -0,0 +1,57 @@
+package bctci.stacks_and_queues
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class Task32_7() {
+
+    fun isValidCustomBrackets(s: String, brackets: Array<String>): Boolean {
+        val openToCloseMap = brackets.associateBy({ it[0] }, { it[1] })
+        val openBrackets = openToCloseMap.keys
+        val closeBrackets = openToCloseMap.values
+        val stack = ArrayDeque<Char>()
+        var expectedCloseCh: Char? = null
+        for (ch in s) {
+            when (ch) {
+                in openBrackets -> {
+                    stack.addFirst(ch)
+                    expectedCloseCh = requireNotNull(openToCloseMap[ch])
+                }
+                expectedCloseCh -> {
+                    stack.removeFirst()
+                    expectedCloseCh = stack.firstOrNull()?.let { requireNotNull(openToCloseMap[it]) }
+                }
+                in closeBrackets -> return false
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
+
+private class Task32_7Test {
+    private val task = Task32_7()
+
+    @ParameterizedTest
+    @MethodSource("inputDataProvider")
+    fun test(input1: String, input2: Array<String>, expected: Boolean) {
+        Assertions.assertEquals(expected, task.isValidCustomBrackets(input1, input2))
+    }
+
+    companion object {
+        @JvmStatic
+        private fun inputDataProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("((a+b)*[c-d]-{e/f})", arrayOf("()", "[]", "{}"), true),
+                Arguments.of("()[}", arrayOf("()", "[]", "{}"), false),
+                Arguments.of("([)]", arrayOf("()", "[]", "{}"), false),
+                Arguments.of("<div> hello :) </div>", arrayOf("<>", "()"), false),
+                Arguments.of(")))(()((", arrayOf(")("), true),
+                Arguments.of("()", arrayOf("()"), true),
+            )
+        }
+    }
+}
