2e6fba3afab19879d6be83b9eefe1aebfa0b784c
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-11-05 19:31:00 -0800

    Formatted 33.2

--- src/main/kotlin/bctci/recursion/Task33.2.kt
+++ src/main/kotlin/bctci/recursion/Task33.2.kt
@@ -12,7 +12,7 @@ class Task33_2() {
     fun calculateNestedArraySum(arr: List<Any>): Int {
         var sum = 0
         for (item in arr) {
-            sum+= item as? Int ?: calculateNestedArraySum(item as List<Int>)
+            sum += item as? Int ?: calculateNestedArraySum(item as List<Int>)
         }
         return sum
     }
@@ -25,33 +25,33 @@ class Task33_2() {
         if (item is Int) return item
         var sum = 0
         for (item in item as List<Any>) {
-            sum+= calculateNestedArraySumLazyRec(item)
+            sum += calculateNestedArraySumLazyRec(item)
         }
         return sum
     }
 }
 
 
-    private class Task33_2Test {
-        private val task = Task33_2()
+private class Task33_2Test {
+    private val task = Task33_2()
 
-        @ParameterizedTest
-        @MethodSource("inputDataProvider")
-        fun test(input: List<Any>, expected: Int) {
-            Assertions.assertEquals(expected, task.calculateNestedArraySumLazy(input))
-        }
+    @ParameterizedTest
+    @MethodSource("inputDataProvider")
+    fun test(input: List<Any>, expected: Int) {
+        Assertions.assertEquals(expected, task.calculateNestedArraySumLazy(input))
+    }
 
-        companion object {
-            @JvmStatic
-            private fun inputDataProvider(): Stream<Arguments> {
-                return Stream.of(
-                    Arguments.of(listOf(1, listOf(2, 3), listOf(4, listOf(5)), 6), 21),
-                    Arguments.of(listOf(listOf(listOf(listOf(1)),2)), 3),
-                    Arguments.of(listOf<Any>(), 0),
-                    Arguments.of(listOf(listOf<Any>(),listOf(1, 2), listOf(),listOf(3)), 6),
-                    Arguments.of(listOf(-1, listOf(-2, 3), listOf(4, listOf(-5)), 6), 5),
-                    Arguments.of(listOf(0, listOf(0, listOf(0, listOf(0))), 0), 0)
-                )
-            }
+    companion object {
+        @JvmStatic
+        private fun inputDataProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(listOf(1, listOf(2, 3), listOf(4, listOf(5)), 6), 21),
+                Arguments.of(listOf(listOf(listOf(listOf(1)), 2)), 3),
+                Arguments.of(listOf<Any>(), 0),
+                Arguments.of(listOf(listOf<Any>(), listOf(1, 2), listOf(), listOf(3)), 6),
+                Arguments.of(listOf(-1, listOf(-2, 3), listOf(4, listOf(-5)), 6), 5),
+                Arguments.of(listOf(0, listOf(0, listOf(0, listOf(0))), 0), 0)
+            )
         }
     }
+}
