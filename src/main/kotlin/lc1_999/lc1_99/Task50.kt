bd732e8c6461b5a8801b7c1dbd88ff741ca27a17
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-11-08 16:26:45 -0800

    Formatted 50

--- src/main/kotlin/lc1_999/lc1_99/Task50.kt
+++ src/main/kotlin/lc1_999/lc1_99/Task50.kt
@@ -11,16 +11,17 @@ import kotlin.math.abs
 //https://leetcode.com/problems/powx-n/
 class Task50() {
     fun myPow(x: Double, n: Int): Double {
-        val newX = if (n<0) 1/x else x
+        val newX = if (n < 0) 1 / x else x
         return powRec(newX, abs(n.toLong()))
     }
-    private fun powRec(x: Double, n: Long): Double{
+
+    private fun powRec(x: Double, n: Long): Double {
         if (n == 0L) return 1.0
-        return if (n % 2 == 0L){
-            val half = powRec(x, n/2)
+        return if (n % 2 == 0L) {
+            val half = powRec(x, n / 2)
             half * half
-        }else{
-            x *  powRec(x, n-1)
+        } else {
+            x * powRec(x, n - 1)
         }
     }
 }
@@ -43,7 +44,7 @@ private class Task50Test {
                 Arguments.of(2.1, 3, 9.261),
                 Arguments.of(2.0, -2, 0.25),
                 Arguments.of(0.44528, 0, 1.0),
-                Arguments.of(2.0,-2147483648, 0.0)
+                Arguments.of(2.0, -2147483648, 0.0)
             )
         }
     }
