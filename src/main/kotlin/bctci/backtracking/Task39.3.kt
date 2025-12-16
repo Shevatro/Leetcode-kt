ce197cd3ca872c2538db2012cb5b2d149062c1c3
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-12-15 17:49:00 -0800

    Solved 39.3

---
+++ src/main/kotlin/bctci/backtracking/Task39.3.kt
@@ -0,0 +1,71 @@
+package bctci.backtracking
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class Task39_3 {
+    fun calcPermutationEnumeration(s: List<Char>): Set<List<Char>> {
+        return Solution(s).calcPermutationEnumeration()
+    }
+
+    private class Solution(private val s: List<Char>) {
+        private val result = mutableSetOf<List<Char>>()
+        private val curSubSet = mutableSetOf<Char>()
+        fun calcPermutationEnumeration(): Set<List<Char>> {
+            backtrack()
+            return result
+        }
+
+        private fun backtrack() {
+            if (curSubSet.size == s.size) {
+                result.add(curSubSet.toList())
+                return
+            }
+            for (ch in s) {
+                if (!curSubSet.contains(ch)) {
+                    curSubSet.add(ch)
+                    backtrack()
+                    curSubSet.remove(ch)
+                }
+            }
+        }
+    }
+}
+
+
+private class Task39_3Test {
+    private val task = Task39_3()
+
+    @ParameterizedTest
+    @MethodSource("inputDataProvider")
+    fun test(input: List<Char>, expected: Set<List<Char>>) {
+        Assertions.assertEquals(expected, task.calcPermutationEnumeration(input))
+    }
+
+    companion object {
+        @JvmStatic
+        private fun inputDataProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    listOf('x', 'y', 'z'),
+                    setOf(
+                        listOf('x', 'y', 'z'), listOf('x', 'z', 'y'), listOf('y', 'x', 'z'),
+                        listOf('y', 'z', 'x'), listOf('z', 'x', 'y'), listOf('z', 'y', 'x')
+                    )
+                ),
+                Arguments.of(listOf('a'), setOf(listOf('a'))),
+                Arguments.of(listOf('a', 'b'), setOf(listOf('a', 'b'), listOf('b', 'a'))),
+                Arguments.of(
+                    listOf('a', 'b', 'c'),
+                    setOf(
+                        listOf('a', 'b', 'c'), listOf('a', 'c', 'b'), listOf('b', 'a', 'c'),
+                        listOf('b', 'c', 'a'), listOf('c', 'a', 'b'), listOf('c', 'b', 'a')
+                    )
+                )
+            )
+        }
+    }
+}
