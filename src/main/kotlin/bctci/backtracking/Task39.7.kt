1ebc3295f08e493cd8cad2d7d70600a1f7e6a4a0
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-12-17 18:46:51 -0800

    Improved 39.7

--- src/main/kotlin/bctci/backtracking/Task39.7.kt
+++ src/main/kotlin/bctci/backtracking/Task39.7.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.util.LinkedList
 import java.util.stream.Stream
 
 class Task39_7 {
@@ -16,18 +17,16 @@ class Task39_7 {
         private val prices: List<Int>,
         private val ratings: List<Double>
     ) {
-        private var curBudget = 0
-        private var curRating = 0.0
         private var maxPickedRating = 0.0
-        private val pickedItems = mutableSetOf<Int>()
+        private val pickedItems = LinkedList<Int>()
         private var maxPickedItems = emptyList<Int>()
 
         fun calcStylishIKEAProduct(): List<Int> {
-            backtrack(0)
+            backtrack(0, 0, 0.0)
             return maxPickedItems
         }
 
-        private fun backtrack(i: Int) {
+        private fun backtrack(i: Int, curBudget: Int, curRating: Double) {
             if (i == prices.size) {
                 if (curRating > maxPickedRating) {
                     maxPickedRating = curRating
@@ -39,20 +38,15 @@ class Task39_7 {
             val newBudget = curBudget + prices[i]
             if (newBudget <= budget) {
                 //do
-                curBudget += prices[i]
-                curRating += ratings[i]
-                pickedItems.add(i)
+                pickedItems.addLast(i)
 
-
-                backtrack(i + 1) // pick
+                backtrack(i + 1, newBudget, curRating + ratings[i]) // pick
 
                 //undo
-                curBudget -= prices[i]
-                curRating -= ratings[i]
-                pickedItems.remove(i)
+                pickedItems.removeLast()
             }
 
-            backtrack(i + 1) // skip
+            backtrack(i + 1, curBudget, curRating) // skip
         }
     }
 }
@@ -75,7 +69,9 @@ private class Task39_7Test {
                 Arguments.of(10, listOf(2, 3, 4, 5), listOf(1.0, 2.0, 3.5, 4.0), listOf(2, 3)),
                 Arguments.of(0, listOf(1, 2, 3), listOf(1.0, 2.0, 3.0), emptyList<Int>()),
                 Arguments.of(10, emptyList<Int>(), emptyList<Double>(), emptyList<Int>()),
-                Arguments.of(50, listOf(10, 20, 30), listOf(10.0, 20.0, 30.0), listOf(1, 2))
+                Arguments.of(50, listOf(10, 20, 30), listOf(10.0, 20.0, 30.0), listOf(1, 2)),
+                //Greedy counterexample
+                Arguments.of(10, listOf(6, 5, 5), listOf(9.0, 8.0, 8.0), listOf(1, 2))
             )
         }
     }
