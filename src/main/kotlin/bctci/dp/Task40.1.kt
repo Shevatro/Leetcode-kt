e5c4de4912f8712fe528653893cd65f6cd91c16c
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-12-22 17:48:54 -0800

    Added more solutions for 40.1

--- src/main/kotlin/bctci/dp/Task40.1.kt
+++ src/main/kotlin/bctci/dp/Task40.1.kt
@@ -8,24 +8,62 @@ import java.util.stream.Stream
 
 class Task40_1 {
     fun calRoadTrip(detours: IntArray): Int {
-        return Solution(detours).calRoadTrip()
+        return SolutionTabulation(detours).calRoadTrip()
     }
 
-    private class Solution(
+    private class SolutionForwardRecursion(
         private val detours: IntArray
     ) {
         private val memo = IntArray(detours.size) { Int.MIN_VALUE }
 
         fun calRoadTrip(): Int {
             if (detours.size <= 2) return 0
-            return listOf(dp(0), dp(1), dp(2)).min()
+            return minOf(dp(0), dp(1), dp(2))
         }
 
         private fun dp(i: Int): Int {
             if (memo[i] != Int.MIN_VALUE) return memo[i]
             if (i in memo.lastIndex - 2..memo.lastIndex) return detours[i]
-            memo[i] = detours[i] + listOf(dp(i + 1), dp(i + 2), dp(i + 3)).min()
+            memo[i] = detours[i] + minOf(dp(i + 1), dp(i + 2), dp(i + 3))
             return memo[i]
+        }
+    }
+
+    private class SolutionBackwardRecursion(
+        private val detours: IntArray
+    ) {
+        private val memo = IntArray(detours.size) { Int.MIN_VALUE }
+
+        fun calRoadTrip(): Int {
+            if (detours.size <= 2) return 0
+            return minOf(dp(memo.lastIndex), dp(memo.lastIndex - 1), dp(memo.lastIndex - 2))
+        }
+
+        private fun dp(i: Int): Int {
+            if (memo[i] != Int.MIN_VALUE) return memo[i]
+            if (i in 0..2) return detours[i]
+            memo[i] = detours[i] + minOf(dp(i - 1), dp(i - 2), dp(i - 3))
+            return memo[i]
+        }
+    }
+
+    //bottom up
+    private class SolutionTabulation(
+        private val detours: IntArray
+    ) {
+
+        fun calRoadTrip(): Int {
+            if (detours.size <= 2) return 0
+            var `i-1` = detours[2]
+            var `i-2` = detours[1]
+            var `i-3` = detours[0]
+            for (i in 3 until detours.size) {
+                val n = detours[i] + minOf(`i-3`, `i-2`, `i-1`)
+                `i-3` = `i-2`
+                `i-2` = `i-1`
+                `i-1` = n
+            }
+            return minOf(`i-1`, `i-2`, `i-3`)
         }
     }
 }
