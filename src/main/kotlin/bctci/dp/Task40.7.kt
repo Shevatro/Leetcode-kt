906e7c3a92b710154ea8e679926bbbaeac2181bb
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-12-30 14:34:31 -0800

    Finished 40.7

--- src/main/kotlin/bctci/dp/Task40.7.kt
+++ src/main/kotlin/bctci/dp/Task40.7.kt
@@ -1,4 +1,4 @@
-package lc1000_1999.lc1000_1099
+package bctci.dp
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
@@ -7,32 +7,30 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 import kotlin.math.max
 
-//Similar to Beyond Cracking The Coding Interview, Not Solved
-//https://leetcode.com/problems/longest-common-subsequence/description/
-class Task1143 {
+class Task40_7 {
     fun longestCommonSubsequence(text1: String, text2: String): Int {
         return Solution(text1, text2).longestCommonSubsequence()
     }
 
     private class Solution(
-        private val text1: String,
-        private val text2: String
+        private val s1: String,
+        private val s2: String
     ) {
-        private val memo = Array(text1.length) { IntArray(text2.length) { -1 } }
+        private val memo = Array(s1.length) { IntArray(s2.length) { -1 } }
 
         fun longestCommonSubsequence(): Int {
             return dp(0, 0)
         }
 
         private fun dp(i1: Int, i2: Int): Int {
-            if (i1 == text1.length || i2 == text2.length) return 0
+            if (i1 == s1.length || i2 == s2.length) return 0
             if (memo[i1][i2] != -1) return memo[i1][i2]
-            memo[i1][i2] = if (text1[i1] == text2[i2]) {
+            memo[i1][i2] = if (s1[i1] == s2[i2]) {
                 //we are good, add this letter to the LCS
                 dp(i1 + 1, i2 + 1) + 1
             } else {
                 //discard text1[i1] or text2[i2],
-                //ex. text1 = "ABC" and text2 = "BCA". It is important that we discard the 'A' in text1, not the 'B' in text2
+                //ex. s1 = "ABC" and s2 = "BCA". It is important that we discard the 'A' in s1, not the 'B' in s2
                 max(dp(i1, i2 + 1), dp(i1 + 1, i2))
             }
             return memo[i1][i2]
@@ -40,8 +38,9 @@ class Task1143 {
     }
 }
 
-private class Task1143Test {
-    private val task = Task1143()
+
+private class Task40_7Test {
+    private val task = Task40_7()
 
     @ParameterizedTest
     @MethodSource("inputDataProvider")
@@ -53,9 +52,15 @@ private class Task1143Test {
         @JvmStatic
         private fun inputDataProvider(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of("abcde", "ace", 3),
-                Arguments.of("abc", "abc", 3),
-                Arguments.of("abc", "def", 0)
+                Arguments.of("HAHAH", "AAAAHH", 3),
+                Arguments.of("", "AA", 0),
+                Arguments.of("ABC", "BCA", 2),
+                Arguments.of("ABCD", "ACBAD", 3),
+                Arguments.of("", "", 0),
+                Arguments.of("ABCDEFGHIJ", "ACBDEFGHIK", 8),
+                Arguments.of("AAAAAAAAAAAAAAA", "AAAAAAAAAAAAA", 13),
+                Arguments.of("THEQUICKBROWNFOX", "THESLOWREDFOX", 8),
+                Arguments.of("AAAAABBBBBCCCCCDDDDD", "BBBBBCCCCCDDDDDEEEEE", 15)
             )
         }
     }
