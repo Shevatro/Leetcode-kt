e18335e4cf03c8d8f0c1eb7307870c9f4e82fede
Sergio Shevatro <sergio.shevatro@gmail.com>
2025-12-29 20:30:00 -0800

    Clean-up 397

--- src/main/kotlin/lc1_999/lc300_399/Task397.kt
+++ src/main/kotlin/lc1_999/lc300_399/Task397.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
-import kotlin.math.min
 
 //Similar to Beyond Cracking The Coding Interview, Solved
 //https://leetcode.com/problems/integer-replacement/
