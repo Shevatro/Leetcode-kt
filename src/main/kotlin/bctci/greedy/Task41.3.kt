b0b7ee053e5bb9a245c280efbc071204cbeef466
Sergio Shevatro <sergio.shevatro@gmail.com>
2026-01-06 15:37:31 -0800

    Improved 41.3

--- src/main/kotlin/bctci/greedy/Task41.3.kt
+++ src/main/kotlin/bctci/greedy/Task41.3.kt
@@ -5,22 +5,25 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 class Task41_3 {
 
     fun minDistanceToCenters(points: Array<DoubleArray>, center1: DoubleArray, center2: DoubleArray): Double {
-        points.sortBy {
-            sqrt(((it[0] - center1[0]) * (it[0] - center1[0])) + ((it[1] - center1[1]) * (it[1] - center1[1]))) -
-                    sqrt(((it[0] - center2[0]) * (it[0] - center2[0])) + ((it[1] - center2[1]) * (it[1] - center2[1])))
-        }
+        // Sort by how much better center1 is than center2 (squared distances)
+        points.sortBy { dist(it, center1) - dist(it, center2) }
         val half = points.size / 2
         var totalDistance = 0.0
         for (i in 0 until half) {
-            totalDistance += sqrt(((points[i][0] - center1[0]) * (points[i][0] - center1[0])) + ((points[i][1] - center1[1]) * (points[i][1] - center1[1])))
-            totalDistance += sqrt(((points[i + half][0] - center2[0]) * (points[i + half][0] - center2[0])) + ((points[i + half][1] - center2[1]) * (points[i + half][1] - center2[1])))
+            totalDistance += dist(points[i], center1)
+            totalDistance += dist(points[i + half], center2)
         }
         return totalDistance
+    }
+
+    private fun dist(point1: DoubleArray, point2: DoubleArray): Double {
+        return sqrt((point1[0] - point2[0]).pow(2) + (point1[1] - point2[1]).pow(2))
     }
 }
 
