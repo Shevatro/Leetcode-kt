2e17d88eb4480100872c3b53e9b7fa28bac310c8
Sergio Shevatro <sergio.shevatro@gmail.com>
2026-02-03 17:29:58 -0800

    Solved 28

---
+++ src/main/kotlin/lc1_999/lc1_99/Task28.kt
@@ -0,0 +1,63 @@
+package lc1_999.lc1_99
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+//From Beyond Cracking The Coding Interview, Solved
+//Note: There are more efficient algorithms like KMP and rolling hash that can solve this in O(sn) time
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+class Task28 {
+    fun strStr(haystack: String, needle: String): Int {
+        return Solution(haystack, needle).strStr()
+    }
+
+    private class Solution(
+        private val haystack: String,
+        private val needle: String
+    ) {
+        fun strStr(): Int {
+            for (i in haystack.indices) {
+                val straw = haystack[i]
+                if (straw == needle[0]) {
+                    if (matches(i)) return i
+                }
+            }
+            return -1
+        }
+
+        private fun matches(startPos: Int): Boolean {
+            if (startPos + needle.length > haystack.length) return false
+            var curNeedleInd = startPos
+            for (needlePart in needle) {
+                if (needlePart != haystack[curNeedleInd]) return false
+                curNeedleInd++
+            }
+            return true
+        }
+    }
+}
+
+private class Task28Test {
+    private val task = Task28()
+
+    @ParameterizedTest
+    @MethodSource("inputDataProvider")
+    fun test(input1: String, input2: String, expected: Int) {
+        Assertions.assertEquals(expected, task.strStr(input1, input2))
+    }
+
+    companion object {
+        @JvmStatic
+        private fun inputDataProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("sadbutsad", "sad", 0),
+                Arguments.of("leetcode", "leeto", -1),
+                Arguments.of("mississippi", "issip", 4),
+                Arguments.of("a", "a", 0)
+            )
+        }
+    }
+}
