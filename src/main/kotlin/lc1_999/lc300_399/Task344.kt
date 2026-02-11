3b7639a066352345e23e1c31b1aefe915e13f4d0
Sergio Shevatro <sergio.shevatro@gmail.com>
2026-02-11 13:58:50 -0800

    Fixed 344

--- src/main/kotlin/lc1_999/lc300_399/Task344.kt
+++ src/main/kotlin/lc1_999/lc300_399/Task344.kt
@@ -1,13 +1,44 @@
 package lc1_999.lc300_399
 
-//Task was changed, repeat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+//From Beyond Cracking The Coding Interview, Solved
 //https://leetcode.com/problems/reverse-string/
 class Task344 {
-    fun reverseString(s: String): String {
-        return StringBuilder(s).reverse().toString()
+    fun reverseString(s: CharArray): Unit {
+        var startPos = 0
+        var endPos = s.lastIndex
+        while (startPos < endPos) {
+            val temp = s[startPos]
+            s[startPos] = s[endPos]
+            s[endPos] = temp
+            startPos++
+            endPos--
+        }
     }
 }
 
-fun main() {
-    println(Task344().reverseString("hello"))
+private class Task344Test {
+    private val task = Task344()
+
+    @ParameterizedTest
+    @MethodSource("inputDataProvider")
+    fun test(input: CharArray, expected: CharArray) {
+        task.reverseString(input)
+        Assertions.assertArrayEquals(expected, input)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun inputDataProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(charArrayOf('h', 'e', 'l', 'l', 'o'), charArrayOf('o', 'l', 'l', 'e', 'h')),
+                Arguments.of(charArrayOf('H', 'a', 'n', 'n', 'a', 'h'), charArrayOf('h', 'a', 'n', 'n', 'a', 'H'))
+            )
+        }
+    }
 }
