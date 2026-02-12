afb410396d6d549f69a907c4e9531ce149529a94
Sergio Shevatro <sergio.shevatro@gmail.com>
2026-02-11 18:31:42 -0800

    Fixed 27.15

--- src/main/kotlin/bctci/two_pointers/Task27.15.kt
+++ src/main/kotlin/bctci/two_pointers/Task27.15.kt
@@ -10,22 +10,66 @@ import kotlin.collections.toList
 class Task27_15 {
 
     fun quicksortPartition(arr: IntArray, pivot: Int): IntArray {
-        var startPos = 0
-        var endPos = arr.lastIndex
-        while (startPos < endPos) {
-            if (arr[startPos] <= pivot) {
-                startPos++
-                continue
-            }
-            if (arr[endPos] > pivot) {
-                endPos--
-                continue
-            }
-            arr[startPos] = arr[endPos].also { arr[endPos] = arr[startPos] }
-            startPos++
-            endPos--
+        return Solution(arr, pivot).quicksortPartition()
+    }
+
+    private class Solution(
+        private val arr: IntArray,
+        private val pivot: Int
+    ) {
+        fun quicksortPartition(): IntArray {
+            applyPartitionSorting()
+            fixDuplicatesPositions()
+            return arr
         }
-        return arr
+
+        private fun applyPartitionSorting() {
+            var startPos = 0
+            var endPos = arr.lastIndex
+            while (startPos < endPos) {
+                if (arr[startPos] <= pivot) {
+                    startPos++
+                    continue
+                }
+                if (arr[endPos] > pivot) {
+                    endPos--
+                    continue
+                }
+                arr[startPos] = arr[endPos].also { arr[endPos] = arr[startPos] }
+                startPos++
+                endPos--
+            }
+        }
+
+        private fun findSmallestItemPosInPart2(): Int {
+            //if all items are greater than pivot => do not waste time
+            if (arr.isEmpty() || arr[0] > pivot) return -1
+
+            for (i in arr.lastIndex downTo 0) {
+                if (arr[i] <= pivot) return i
+            }
+            return -1
+        }
+
+        //if we have [1, 3, 2, 3, 3, 5, 7] where pivot=3, we need to change it to [1, 2, 3, 3, 3, 5, 7]
+        private fun fixDuplicatesPositions() {
+            var startPos = 0
+            var endPos = findSmallestItemPosInPart2()
+            if (endPos == -1) return
+            while (startPos < endPos) {
+                if (arr[startPos] < pivot) {
+                    startPos++
+                    continue
+                }
+                if (arr[endPos] == pivot) {
+                    endPos--
+                    continue
+                }
+                arr[startPos] = arr[endPos].also { arr[endPos] = arr[startPos] }
+                startPos++
+                endPos--
+            }
+        }
     }
 }
 
@@ -51,7 +95,7 @@ private class Task27_15Test {
                 ),
                 Arguments.of(
                     intArrayOf(1, 7, 2, 3, 3, 5, 3), 3,
-                    listOf(intArrayOf(1, 2, 3, 3, 3, 7, 5), intArrayOf(1, 3, 2, 3, 3, 5, 7))
+                    listOf(intArrayOf(1, 2, 3, 3, 3, 7, 5), intArrayOf(1, 2, 3, 3, 3, 5, 7))
                 ),
                 Arguments.of(intArrayOf(3, 3, 3, 3), 3, listOf(intArrayOf(3, 3, 3, 3))),
                 Arguments.of(intArrayOf(1, 2, 4, 5), 3, listOf(intArrayOf(1, 2, 4, 5))),
@@ -61,7 +105,7 @@ private class Task27_15Test {
                 Arguments.of(intArrayOf(1), 1, listOf(intArrayOf(1))),
                 Arguments.of(
                     intArrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5), 3,
-                    listOf(intArrayOf(3, 1, 3, 1, 2, 9, 5, 6, 5, 4, 5))
+                    listOf(intArrayOf(2, 1, 1, 3, 3, 9, 5, 6, 5, 4, 5))
                 ),
                 Arguments.of(intArrayOf(1, 2, 3), 4, listOf(intArrayOf(1, 2, 3))),
                 Arguments.of(intArrayOf(5, 6, 7), 4, listOf(intArrayOf(5, 6, 7)))
